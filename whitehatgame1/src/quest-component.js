f71c99f2ef0bde70028aceaa3008ef545e56ff51
perenSHERE <dhruvkaushik170@gmail.com>
2021-01-30 13:14:52 +0530

    check readme.md

---
+++ whitehatgame1/src/quest-component.js
@@ -0,0 +1,40 @@
+import {entity} from "./entity.js";
+
+
+export const quest_component = (() => {
+
+  const _TITLE = 'Welcome Adventurer!';
+  const _TEXT = `Welcome to the jungle Cuz that's all I can make, kill 30 monsters the game ends bye`;
+
+  class QuestComponent extends entity.Component {
+    constructor() {
+      super();
+
+      const e = document.getElementById('quest-ui');
+      e.style.visibility = 'hidden';
+    }
+
+    InitComponent() {
+      this._RegisterHandler('input.picked', (m) => this._OnPicked(m));
+    }
+
+    _OnPicked(msg) {
+      // HARDCODE A QUEST
+      const quest = {
+        id: 'foo',
+        title: _TITLE,
+        text: _TEXT,
+      };
+      this._AddQuestToJournal(quest);
+    }
+
+    _AddQuestToJournal(quest) {
+      const ui = this.FindEntity('ui').GetComponent('UIController');
+      ui.AddQuest(quest);
+    }
+  };
+
+  return {
+      QuestComponent: QuestComponent,
+  };
+})();
